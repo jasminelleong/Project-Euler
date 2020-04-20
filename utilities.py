85df3a2745b2b6edc708521e6c4b7543d409c4ed
Robert Amour <robert@ferretech.io>
2020-04-19 21:52:43 -0700

    Add is_prime utility function

--- utilities.py
+++ utilities.py
@@ -1,3 +1,6 @@
+import math
+
+
 def fibonacci_sequence():
     second_previous_value = 1
     previous_value = 2
@@ -8,3 +11,19 @@ def fibonacci_sequence():
         second_previous_value = previous_value
         previous_value = next_value
         yield next_value
+
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(2, num):
+        if i*i > num:
+            break
+        if num % i == 0:
+            return False
+
+    return True
