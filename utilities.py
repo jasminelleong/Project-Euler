11fc820b1d77ab9e06ab79b8057ec891481a89a0
Robert Amour <robert@ferretech.io>
2020-04-19 20:23:23 -0700

    Add fibonacci sequence generator

---
+++ utilities.py
@@ -0,0 +1,10 @@
+def fibonacci_sequence():
+    second_previous_value = 1
+    previous_value = 2
+    yield second_previous_value
+    yield previous_value
+    while True:
+        second_previous_value = previous_value
+        next_value = second_previous_value + previous_value
+        previous_value = next_value
+        yield next_value
