a160a55d3d278b3d79ad82c8a9afaa20517fdbfa
Robert Amour <robert@ferretech.io>
2020-04-21 19:24:32 -0700

    Add 'integer to words' utility function

--- utilities.py
+++ utilities.py
@@ -77,3 +77,82 @@ def collatz_step(num):
     if num % 2 == 0:
         return num // 2
     return (num * 3) + 1
+
+
+def integer_to_words(number):
+    def digit_to_word(digit):
+        if digit == 0:
+            return ""
+        if digit == 1:
+            return "one"
+        if digit == 2:
+            return "two"
+        if digit == 3:
+            return "three"
+        if digit == 4:
+            return "four"
+        if digit == 5:
+            return "five"
+        if digit == 6:
+            return "six"
+        if digit == 7:
+            return "seven"
+        if digit == 8:
+            return "eight"
+        if digit == 9:
+            return "nine"
+
+    if number == 1000:
+        return "one thousand"
+    if number >= 100:
+        partial_answer = digit_to_word(number // 100) + ' hundred '
+        if number % 100 != 0:
+            partial_answer += 'and '
+            return partial_answer + integer_to_words(number % 100)
+
+    if number >= 20:
+        if number <= 29:
+            partial_answer = "twenty"
+        elif number <= 39:
+            partial_answer = "thirty"
+        elif number <= 49:
+            partial_answer = "forty"
+        elif number <= 59:
+            partial_answer = "fifty"
+        elif number <= 69:
+            partial_answer = "sixty"
+        elif number <= 79:
+            partial_answer = "seventy"
+        elif number <= 89:
+            partial_answer = "eighty"
+        elif number <= 99:
+            partial_answer = "ninety"
+
+        if number % 10 != 0:
+            partial_answer += '-' + digit_to_word(number % 10)
+
+        return partial_answer
+
+    if number < 10:
+        return digit_to_word(number)
+
+    if number == 10:
+        return "ten"
+    if number == 11:
+        return "eleven"
+    if number == 12:
+        return "twelve"
+    if number == 13:
+        return "thirteen"
+    if number == 14:
+        return "fourteen"
+    if number == 15:
+        return "fifteen"
+    if number == 16:
+        return "sixteen"
+    if number == 17:
+        return "seventeen"
+    if number == 18:
+        return "eighteen"
+    if number == 19:
+        return "nineteen"
